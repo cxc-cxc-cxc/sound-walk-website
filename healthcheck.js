abc48612c83a460ba5c40ac3fa5f2c036d8b072b
cxc-cxc-cxc <carlosc.clar@gmail.com>
2026-03-24 12:53:31 +0100

    chore(deploy): healthcheck, yarn nodeLinker, update start script for 8080

---
+++ healthcheck.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Healthcheck OK\n');
+});
+
+server.listen(port, '0.0.0.0', () => {
+  console.log(`==== HEALTHCHECK SERVER UP ON PORT ${port} ====`);
+});
